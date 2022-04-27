a9b7c1ccbdd6b6b14070172bc4f2bae61627e8bc
milo <mybule8@naver.com>
2022-04-27 23:38:36 +0900

    5.회원가입 완료

--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,6 +1,7 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.service.UserService;
@@ -9,4 +10,14 @@ import site.metacoding.blogv2.service.UserService;
 @Controller
 public class UserController {
     private final UserService userService;
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
 }
